9dee8f629daa27de25c180b8e1b8a8d987458ed4
Dzmitriy Kaminski <dk@Dzmitriys-MacBook-Air.local>
2023-10-26 18:12:59 +0300

    Fix block physical buttons

--- app/src/main/java/com/vlr/dk/yugratablet/HomeFragment.kt
+++ app/src/main/java/com/vlr/dk/yugratablet/HomeFragment.kt
@@ -16,9 +16,7 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
-        view.findNavController()
-        return view
+        return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
 
--- app/src/main/java/com/vlr/dk/yugratablet/MainActivity.kt
+++ app/src/main/java/com/vlr/dk/yugratablet/MainActivity.kt
@@ -2,22 +2,26 @@ package com.vlr.dk.yugratablet
 
 import android.app.ActivityManager
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val am = getSystemService(
-            ACTIVITY_SERVICE
-        ) as ActivityManager
-        if (am.lockTaskModeState ==
-            ActivityManager.LOCK_TASK_MODE_NONE
-        ) {
-            startLockTask()
-        }
-
         this.supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val activityManager = applicationContext
+            .getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        activityManager.moveTaskToFront(taskId, 0)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+       return false
     }
 }
